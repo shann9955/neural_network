6a8f007d31741fb3cda27649f9bb97c4e1b10d55
shann9955 <shannonkelly955@hotmail.co.uk>
2019-03-12 13:01:21 +0000

    Testing sklearn and adding different algorithms

--- model.py
+++ model.py
@@ -1,16 +1,21 @@
 from keras.models import load_model
 from numpy import array as np
+import numpy as nump
+from time import sleep
 
-def predictModel():
+#TODO: check input
+#TODO: celery to pause ml moves (for experience)
 
+def predictModel(categories):
 
     #using the trained model to predict
-    model = load_model('model.h5')
 
-    categories=["Graphics cards", "Monitors", "Processors", "Mouse", "Keyboards"]
+    #categories=["Graphics cards", "Monitors", "Processors", "Mouse", "Keyboards"]
 
     num_cat=[]
+    prediction=[]
 
+    #TODO: change this to search for the tags/categories? (Data which user clicks on)
     for cat in categories:
         if cat=="Graphics cards":
             num_cat.append("1")
@@ -22,6 +27,8 @@ def predictModel():
             num_cat.append("4")
         if cat=="Keyboards":
             num_cat.append("5")
+        else:
+            num_cat.append("6")
 
     while len(num_cat)<18:
         num_cat.append("0")
@@ -30,37 +37,63 @@ def predictModel():
     num_cat_numpy.append(num_cat)
     num_cat_numpy=np(num_cat_numpy)
 
-    prediction=model.predict(num_cat_numpy).argmax()
-    print(prediction)
+    num_cat_numpy = nump.reshape(num_cat_numpy, (num_cat_numpy.shape[0], num_cat_numpy.shape[1]))
 
-    def NumberToPredCat(num):
+    print("0: ", num_cat_numpy.shape[0], "1: ", num_cat_numpy.shape[1])
+
+    prediction=model.predict(num_cat_numpy).argmax()
+    print("Prediction number: ", prediction)
+    
+    predict=NumberToPredCat(prediction)
+    #predict=InvertPred(cat)
+
+    return InvertPred(predict)
+
+def NumberToPredCat(num):
         if num==1:
             return "Graphics cards"
         elif num==2:
             return "Monitors"
         elif num==3:
-            return "Processors"
+             return "Processors"
         elif num==4:
             return "Mouse"
         elif num==5:
             return "Keyboards"
+        else:
+            return "Shannon"
         
-    def InvertPred(cat):
-        if cat=="Graphics cards":
-            return "GC"
-        elif cat=="Monitors":
-            return "M"
-        elif cat=="Processors":
-            return "P"
-        elif cat=="Mouse":
-            return "M"
-        elif cat=="Keyboards":
-            return "K"
-        
-    move=NumberToPredCat(prediction)
-    move=InvertPred(cat)
-    print(move)
+def InvertPred(cat):
+    if cat=="Graphics cards":
+        return "Graphics cards"
+    elif cat=="Monitors":
+        return "Monitors"
+    elif cat=="Processors":
+        return "Processors"
+    elif cat=="Mouse":
+        return "Mouse"
+    elif cat=="Keyboards":
+        return "Keyboards"
 
-#TODO: check input
-#TODO: celery to pause ml moves (for experience)
+def getPrediction(categories):
+    return predictModel(categories)
+
+model = load_model('model.h5')
+
+categories =[]
+
+#TODO: use this as manual for now, maybe change to an input, but update as clicked tag in the future
+user_category = "Processors"
+#user_category = input("Your component ")
+
+categories.append(user_category)
+
+print ("Your category: ", "".join(str(c) for c in categories))
+
+predicted_cat=getPrediction(categories)
+
+#TODO: use celery
+sleep(0.5)
+
+print ("Prediction: ", predicted_cat)
 
--- predictions.py
+++ predictions.py
@@ -8,23 +8,24 @@ from keras import callbacks
 from sklearn.model_selection import train_test_split
 import numpy
 
-dataset = numpy.loadtxt("moves.csv", delimiter=",", skiprows=1)
+dataset = numpy.loadtxt("components2.csv", delimiter=",", skiprows=1)#TODO: csv of categories to predict
 X = dataset[:,0:18]
 Y = dataset[:, 18]
 
 Y=to_categorical(Y)
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.25)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.15)
 
 model = Sequential()
 
-model.add(Dense(64, input_dim=18, activation='relu'))
-model.add(Dense(18, activation='relu'))
+model.add(Dense(18, input_dim=18, activation='relu'))
+model.add(Dense(12, activation='relu'))
 model.add(Dense(9, activation='relu'))
-model.add(Dense(3, activation='relu'))
-model.add(Dense(4, activation='softmax'))
+# model.add(Dense(9, activation='sigmoid'))
+# model.compile(loss='binary_crossentropy', optimizer='adam')
+model.add(Dense(7, activation='softmax'))
 
 savebest=callbacks.ModelCheckpoint(filepath='checkpoint-{val_acc:.2f}.h5',monitor='val_acc',save_best_only=True)
-ReduceLR=callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.75, verbose=0, mode='auto', cooldown=30, min_lr=0.0001)
+ReduceLR=callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.75, verbose=0, mode='auto', cooldown=30, min_lr=0.0001, learning_rate='constant', power_t=0.5, shuffle=True, tol=0.0001)
 class MyCallback(callbacks.Callback):
     def on_epoch_end(self,epoch,logs=None):
         print(K.eval(self.model.optimizer.lr))
